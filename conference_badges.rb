3da2a57ff0e9d6dba9a387d9b5392c0340211ace
ruthnewman <ruthnewman@gmail.com>
2018-11-30 01:49:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+def badge_maker(name)
+  message = "Hello, my name is #{name}."
+  message
+end 
+
+
+def batch_badge_creator(names)
+  badges = []
+  names.each do |name|
+    badges << badge_maker(name)
+  badges
+  end 
+end 
+
+def assign_rooms
+  
+end
+
+def printer
+  
+end 
